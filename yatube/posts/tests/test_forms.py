49eb89d925b9df72ed6fad7e539c2293944aea52
ursmoscow <61637040+ursmoscow@users.noreply.github.com>
2023-04-17 22:28:52 +0300

    review2tests3

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -107,7 +107,7 @@ class PostCreateFormTests(TestCase):
                                                {'text': new_text,
                                                 'group': self.group.id})
         self.assertRedirects(response, reverse(
-            'post',
+            'posts:post',
             kwargs={'username': self.test_user.username,
                     'post_id': self.post.id}))
         self.post.refresh_from_db()
