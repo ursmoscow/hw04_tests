31638cca12f00f51b1c89f87c71f6ad4ef7ecc90
ursmoscow <61637040+ursmoscow@users.noreply.github.com>
2023-04-17 21:39:00 +0300

    Review2pep

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -97,8 +97,7 @@ class PostCreateFormTests(TestCase):
     def test_post_edit_by_author(self):
         edit_url = reverse('post_edit', kwargs={
             'username': self.test_user.username,
-            'post_id': self.post.id
-            })
+            'post_id': self.post.id})
         response = self.authorized_client.get(edit_url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Редактирование поста')
